a408f97b0be38b75b529c5f391b79cf4fc91df60
Cristian Sancricca <cristiansancricca@gmail.com>
2021-11-06 02:25:52 -0300

    Simon dice

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const d = document;
 const $btnJugar = d.querySelector(".btn-jugar");
 const $cuadros = d.querySelectorAll(".cuadro");
 
+let secuenciaMaquinaAComparar = [];
 let secuenciaMaquina = [];
 let secuenciaJugador = [];
 let ronda = 0;
+let mejorRonda = localStorage.getItem("puntaje") || 0;
 
 $btnJugar.addEventListener("click", () => {
   iniciarJuego();
@@ -14,28 +16,74 @@ const iniciarJuego = () => {
   bloquearUsuario();
 
   const $cuadroMaquina = obtenerCuadroAleatorioMaquina();
-  secuenciaMaquina.push($cuadroMaquina.dataset.color);
-  console.log(secuenciaMaquina);
+  secuenciaMaquinaAComparar.push($cuadroMaquina.dataset.color);
+  secuenciaMaquina.push($cuadroMaquina);
 
-  iluminarCuadroMaquina($cuadroMaquina);
+  secuenciaMaquina.forEach((cuadro, i) => {
+    const retraso = (i + 1) * 1000;
+    setTimeout(() => {
+      iluminarCuadro(cuadro);
+    }, retraso);
+  });
+
+  const retrasoJugador = (secuenciaMaquina.length + 1) * 1000;
+
+  setTimeout(() => {
+    desbloquearUsuario();
+  }, retrasoJugador);
+  ronda++;
+  mejorRonda = ronda;
 };
 
 const obtenerCuadroAleatorioMaquina = () => {
-  const indice = Math.floor(Math.random() * $cuadros.length);
-  return $cuadros[indice];
+  const cuadroElegido = Math.floor(Math.random() * $cuadros.length);
+  return $cuadros[cuadroElegido];
 };
 
-const iluminarCuadroMaquina = (e) => {
+const manejarJuegoUsuario = (e) => {
+  const $cuadro = e.target;
+  iluminarCuadro($cuadro);
+  secuenciaJugador.push($cuadro.dataset.color);
+
+  const esperarJugador =
+    secuenciaMaquinaAComparar.length === secuenciaJugador.length;
+
+  if (esperarJugador) {
+    if (
+      JSON.stringify(secuenciaMaquinaAComparar) ===
+      JSON.stringify(secuenciaJugador)
+    ) {
+      secuenciaJugador = [];
+      bloquearUsuario();
+      setTimeout(() => {
+        iniciarJuego();
+      }, 1000);
+    } else {
+      reiniciarJuego();
+    }
+  }
+};
+const iluminarCuadro = (e) => {
   e.classList.add("resaltar-color");
-  secuenciaJugador.push(e.dataset.color); //ESTO IRIA EN EL MANEJO DEL JUGADOR
-  console.log(secuenciaJugador);
+  setTimeout(() => {
+    e.classList.remove("resaltar-color");
+  }, 500);
 };
-$cuadros.forEach((cuadro) => {
-  cuadro.onclick = iluminarCuadro;
-});
 
+const desbloquearUsuario = () => {
+  $cuadros.forEach((cuadro) => {
+    cuadro.onclick = manejarJuegoUsuario;
+  });
+};
 const bloquearUsuario = () => {
   $cuadros.forEach((cuadro) => {
     cuadro.onclick = function () {};
   });
+};
+
+const reiniciarJuego = () => {
+  secuenciaMaquina = [];
+  secuenciaMaquinaAComparar = [];
+  secuenciaJugador = [];
+  bloquearUsuario();
 };
